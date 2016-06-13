904b166e1c207be7f5f681f7d1da379bab187b2f
Tyler <shed412@gmail.com>
2016-06-13 17:36:26 -0400

    Added the files for the 7 leds and put led.js in its own file

--- led.js
+++ led.js
@@ -1,19 +1,20 @@
 
 
 var channel = "disco";
+var sendStr = "";
 var button = document.getElementById('butt');
 
-// var pub = PUBNUB.init({
-//   subscribe_key: 'lights',
-//   publish_key: 'lightswitch'
-// });
+ var pub = PUBNUB.init({
+   subscribe_key: 'sub-c-73e6721a-304c-11e6-8bc8-0619f8945a4f',
+  publish_key: 'pub-c-3ef60582-c7a1-498f-8aed-5a34d2265af5'
+ });
 
 function disco(){
-  alert("Working");
-  // p.publish({
-  //   channel: channel,
-  //   message: {led: 1}
-  // });
+   sendStr = document.getElementById('mess').value;
+   pub.publish({
+     channel: channel,
+     message: {"led": 1, "mess": sendStr}
+   });
 }
 
 if(button){
